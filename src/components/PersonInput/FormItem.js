cb1786be077b53c7fab36cb8e84f09de63d17cfa
chaitanya.ms <chaitanms@gmail.com>
2021-05-12 22:33:37 +0530

    reset userinputs once we click add button

--- src/components/PersonInput/FormItem.js
+++ src/components/PersonInput/FormItem.js
@@ -1,21 +1,16 @@
 import {useState} from 'react';
 
-
 const FormItem = (props) => {
-
   const [userInput, setUserInput] = useState('');
-
-  const userInputChangeHandler = (event) => {
-    setUserInput(event.target.value);
-    props.onChange(event.target.value)
-  };
 
   return (
     <div>
       <label htmlFor={props.label}>{props.label}</label>
-      <input id={props.label} type={props.type} value={userInput} onChange={userInputChangeHandler}></input>
+      <input
+        id={props.label}
+        type={props.type}
+        ref={props.reference}/>
     </div>
   );
 };
-
 export default FormItem;
--- src/components/PersonInput/PersonInput.js
+++ src/components/PersonInput/PersonInput.js
@@ -3,24 +3,26 @@ import FormItem from './FormItem';
 import Card from '../GUICommon/Card';
 import Button from '../GUICommon/Button';
 import ErrorDialog from '../GUICommon/ErrorDialog';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 
 const PersonInput = (props) => {
 
   const [error, setError] = useState('');
-  let name;
-  let age;
+  const nameRef = useRef();
+  const ageRef = useRef();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if(name.trim().length === 0 || age.trim().length === 0) {
+    const enteredName = nameRef.current.value;
+    const enteredAge = ageRef.current.value;
+    if(enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: 'Invalid Input',
         message: 'Please enter valid name and age (non empty values).'
       });
       return;
     }
-    if (+age < 1) {
+    if (+enteredAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter the age above 1'
@@ -28,24 +30,18 @@ const PersonInput = (props) => {
       return;
     }
     props.onAddUser({
-      name : name,
-      age : age,
-      key : Math.random() + age
+      name : enteredName,
+      age : enteredAge,
+      key : Math.random() + enteredAge
     });
+
+    nameRef.current.value = '';
+    ageRef.current.value = '';
   };
 
   const errorHandler = () => {
     setError(null);
   }
-
-  const onNameChangeHandler = (inputValue) => {
-      name = inputValue;
-      console.log(inputValue);
-  };
-
-  const onAgeChangeHandler = (inputValue) => {
-      age = inputValue;
-  };
 
   return (
     <div>
@@ -53,8 +49,8 @@ const PersonInput = (props) => {
     <Card className={styles.input}>
     <form onSubmit={addUserHandler}>
       <div>
-        <FormItem label="Name : " type="text" onChange={onNameChangeHandler}/>
-        <FormItem label="Age : " type="number" onChange={onAgeChangeHandler}/>
+        <FormItem label="Name : " type="text" reference={nameRef}/>
+        <FormItem label="Age : " type="number" reference={ageRef}/>
       </div>
       <Button type="submit">Add User</Button>
     </form>
